2172346852b0a9c9e989cdcb2335211aeb0f5ce6
YeXiuPH <kirivps@gmail.com>
2023-09-11 01:34:03 +0800

    ABI update for demo.

--- RustLink.cpp
+++ RustLink.cpp
@@ -11,44 +11,23 @@
 */
 
 extern "C" {
-    const char* create_key();
-    void* yx_games_key_create();
+    void* yx_games_key_create(const char* EncryptionKey);
     const char* yx_games_key_encrypt(void* YXEncryptionPTR, const char* Data);
     const char* yx_games_key_decrypt(void* YXEncryptionPTR, const char* Data);
     void yx_games_key_destroy(void* YXEncryptionPTR);
 }
 
 int main() {
-    // Get the encrypted key from Server Side
-    const char* encryptedKey = create_key();
-    std::cout << "Encrypted Key: " << encryptedKey << std::endl;
-    const char* expectedKey = "SKyZdHJzsjKZoltLz6MTYrKQjzEwC5FJ2x0n2mJPEGc=";
-
-    // Compare the obtained key with the expected key
-    if (strcmp(encryptedKey, expectedKey) == 0) {
-        std::cout << "Encrypted Key matches the expected value." << std::endl;
-    }
-    else {
-        std::cout << "Encrypted Key does not match the expected value." << std::endl;
-    }
-
+    // This is loaded in first connect - YX Games Login
+    const char* YXKey = "TestEncryption";
     // Create a PTR for YXGames Encryption
-    void* YXEncryptionPTR = yx_games_key_create();
+    void* YXEncryptionPTR = yx_games_key_create(YXKey);
+
     const char* input = "Hello, YXGames Server!";
 
     // Encrypt the input data
     const char* encryptedInput = yx_games_key_encrypt(YXEncryptionPTR, input);
     std::cout << "Encrypted Input: " << encryptedInput << std::endl;
-
-    const char* expectedInput = "4hNit7i5+xpzYIbB7mb1U80jOQVbDh6hUAhSqn+CjI8=";
-
-    // Compare the obtained encrypted input with the expected input
-    if (strcmp(encryptedInput, expectedInput) == 0) {
-        std::cout << "Encrypted Input matches the expected value." << std::endl;
-    }
-    else {
-        std::cout << "Encrypted Input does not match the expected value." << std::endl;
-    }
 
     // Decrypt the input data
     const char* decryptedInput = yx_games_key_decrypt(YXEncryptionPTR, encryptedInput);
